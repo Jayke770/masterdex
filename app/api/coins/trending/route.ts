1fd85a6ab5eeca445cbc8f14623aebe6ed074faa
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-12-01 19:59:49 +0800

    Update route.ts

--- app/api/coins/trending/route.ts
+++ app/api/coins/trending/route.ts
@@ -1,5 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { coingeckoClient } from '@lib/coingecko'
+export const revalidate = 60 * 120;
 export async function GET(req: NextRequest) {
     try {
         const data = await coingeckoClient.trending()
