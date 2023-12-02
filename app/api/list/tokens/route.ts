6ab132d47864f4d86474e9d2819980db762deb43
jayke770 <jakevelasco40@gmail.com>
2023-12-02 21:18:31 +0800

    .

--- app/api/list/tokens/route.ts
+++ app/api/list/tokens/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: NextRequest) {
         const isValidChain = !!SUPPORTED_CHAINS.find(x => x === chainId)
         const tokenListEndpoint = isValidChain ? process.env?.[`TOKEN_LIST_CHAINID_${chainId}`]! : process.env?.TOKEN_LIST_CHAINID_1!
         const data: { tokens: IListToken[] } = await fetch(tokenListEndpoint).then(res => res.json())
-        return NextResponse.json({ tokens: data?.tokens })
+        return NextResponse.json(data?.tokens ?? [])
     } catch (e: any) {
         return NextResponse.json({ messsage: e.message }, { status: 500 })
     }
